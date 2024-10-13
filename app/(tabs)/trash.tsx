a107680b7f8bff672f7831c7de73bab9b364bcfa
Kattis91 <katya.svedmark@gmail.com>
2024-10-13 13:01:13 +0200

    Create navigation for 'Trash' tab

--- app/(tabs)/trash.tsx
+++ app/(tabs)/trash.tsx
@@ -1,9 +1,14 @@
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import { Text, View } from "react-native";
+import ExpiredProducts from "../ExpiredProducts";
 
-export default function HomeScreen() {
-    return (
-      <View>
-        <Text>Hej</Text>
-      </View>
-    );
-  }
+export default function TrashScreen() {
+
+  const Stack = createNativeStackNavigator();
+
+  return (
+     <Stack.Navigator>
+      <Stack.Screen name="Expired Products" component={ExpiredProducts} />     
+    </Stack.Navigator>
+  );
+}
