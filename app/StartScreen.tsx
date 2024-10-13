7d65336ba0db77616e12582599ef67ca3214d528
Kattis91 <katya.svedmark@gmail.com>
2024-10-13 13:03:59 +0200

    Add some style to the screen

--- app/StartScreen.tsx
+++ app/StartScreen.tsx
@@ -1,16 +1,21 @@
 import { Button, Text, View } from "react-native";
 
 export default function StartScreen({ navigation } : any) {
-  return(
-    <View>
-      <Text>FreshAlert</Text>
-      <Text>Keep it Fresh, Keep it Safe</Text>
-
-      <Button 
-        title="GET STARTED"
-        onPress={() => {
+    
+  return (
+    <View style={{ flex: 1, justifyContent: "center", alignItems: "center", backgroundColor: "#f8edeb" }}>
+      <Text style={{ fontSize: 50, color: "#003366" }}>FreshAlert</Text>
+      <Text style={{ fontSize: 28, color: "#003366" }}>Keep it Fresh, Keep it Safe</Text>
+  
+      <View style={{ marginTop: 35, backgroundColor: "#0A7763" }}>
+        <Button 
+          title="GET STARTED" 
+          color="white"
+          onPress={() => {
             navigation.navigate("Get Started");
-        }} />
+          }}
+          />
+      </View>
     </View>
-  );
-}
+    );
+  }
