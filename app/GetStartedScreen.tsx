9b7ad219b1b07f7cb77ec640282fd5db0982562c
Kattis91 <katya.svedmark@gmail.com>
2024-10-13 13:05:59 +0200

    Add content to the screen

--- app/GetStartedScreen.tsx
+++ app/GetStartedScreen.tsx
@@ -1,9 +1,34 @@
-import { Text, View } from "react-native";
+import { Button, Text, View } from "react-native";
 
 export default function GetStartedScreen({ navigation }) {
+  
   return(
-    <View>
-      <Text>Hej</Text>
+    <View style={{ flex: 1, justifyContent: "center", paddingHorizontal: 30 }}>
+
+      <Text style={{ textAlign: "center", fontSize: 45, marginBottom: 35 }}>Welcome to FreshAlert!</Text>
+      
+      <Text style={{ fontSize: 20, marginBottom: 15 }}>
+        Keep your food fresh for longer and avoid unnecessary food waste with our smart app.
+      </Text>
+      <Text style={{ fontSize: 20, marginBottom: 15 }}>
+        FreshAlert helps you easily keep track of the expiration dates of your refrigerated and frozen goods.
+      </Text>
+      <Text style={{ fontSize: 20, marginBottom: 15 }}>
+        Receive timely reminders and plan better meals - all to save both money and the environment.
+      </Text>
+      <Text style={{ fontSize: 20, marginBottom: 15 }}>
+        Get started by adding your items, and we'll take care of the rest! Let FreshAlert make your refrigerator management easier and smarter.
+      </Text>
+
+      <View style={{ marginTop: 15, backgroundColor: "#0A7763" }}>
+        <Button 
+          title="ADD PRODUCT" 
+          color="white"
+          onPress={() => {
+            navigation.navigate("add", { screen: "Add Product" });
+          }}
+        />
+      </View>
     </View>
   );
 }
